0b55d06fe0c626c8c2b509aab9d927b52aa7c7bb
Le Thanh Hai <hailt@exoplatform.com>
2012-06-11 10:40:42 +0700

    COMMONS-154  update junit test case for DataInjector

--- component/common/src/test/java/org/exoplatform/services/bench/TestDataInjector.java
+++ component/common/src/test/java/org/exoplatform/services/bench/TestDataInjector.java
@@ -17,6 +17,9 @@
 package org.exoplatform.services.bench;
 
 
+import java.util.HashMap;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 /**
@@ -29,9 +32,15 @@ public class TestDataInjector extends TestCase {
   
   FakeDataInjector dataInjector;
   
+  HashMap<String, String> queryParams;
+  
   @Override
   protected void setUp() throws Exception {
     dataInjector = new FakeDataInjector();
+    queryParams = new HashMap<String, String>();
+    queryParams.put("groups", "validator:/platform/administrators,manager:/platform/users");
+    queryParams.put("users", "root,john,marry,james,demo");
+    queryParams.put("memship", "manager,member,validator");
     super.setUp();
   }
 
@@ -51,4 +60,46 @@ public class TestDataInjector extends TestCase {
     assertTrue(dataInjector.createTextResource(10).getBytes().length == (10 * 1024));
   }
   
+  /**
+   * Test function: DataInjector.readGroupsIfExist()
+   */
+  public void testReadGroupsIfExist() {
+    List<String> groups = dataInjector.readGroupsIfExist(queryParams);
+    assertNotNull(groups);
+    assertEquals(2, groups.size());
+    assertEquals("validator:/platform/administrators", groups.get(0));
+    assertEquals("manager:/platform/users", groups.get(1));
+  }
+  
+  /**
+   * Test function: DataInjector.readUsersIfExist()
+   */
+  public void testReadUsersIfExist() {
+    List<String> users = dataInjector.readUsersIfExist(queryParams);
+    assertNotNull(users);
+    assertEquals(5, users.size());
+    assertEquals("root", users.get(0));
+    assertEquals("john", users.get(1));
+    assertEquals("marry", users.get(2));
+    assertEquals("james", users.get(3));
+    assertEquals("demo", users.get(4));    
+  }
+  
+  /**
+   * Test function: DataInjector.readMembershipIfExist()
+   */
+  public void testReadMembershipIfExist() {
+    List<String> memberships = dataInjector.readMembershipIfExist(queryParams);
+    assertNotNull(memberships);
+    assertEquals(3, memberships.size());
+    assertEquals("manager", memberships.get(0));
+    assertEquals("member", memberships.get(1));
+    assertEquals("validator", memberships.get(2));
+  }
+  
+  protected void tearDown() throws Exception {
+    dataInjector = null;
+    queryParams = null;
+  }  
 }
+
