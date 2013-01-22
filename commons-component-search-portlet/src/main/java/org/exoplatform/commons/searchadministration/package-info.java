d68b5e956059daf23e24065960b8b3e326c4719e
Canh Pham Van <canhpv@exoplatform.com>
2013-01-22 18:32:39 +0700

    COMMONS-190: implemenet portlet singleton for search administration

---
+++ commons-component-search-portlet/src/main/java/org/exoplatform/commons/searchadministration/package-info.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2003-2012 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+@Application
+@Portlet
+@Assets(
+        scripts =  @Script(id = "jquery", src = "jquery-1.7.1.min.js", location = AssetLocation.CLASSPATH)
+) package org.exoplatform.commons.searchadministration;
+
+import juzu.Application;
+import juzu.asset.AssetLocation;
+import juzu.plugin.asset.Assets;
+import juzu.plugin.asset.Script;
+import juzu.plugin.portlet.Portlet;
