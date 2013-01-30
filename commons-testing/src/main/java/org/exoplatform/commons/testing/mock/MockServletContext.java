c0a2d73ec2d0043b3cf6e93b3d45f759caa47c8a
Frederic DROUET <fdrouet@exoplatform.com>
2013-01-30 01:07:54 +0100

    PLF-4229 + DEP-12 + DEP-13 : Add a KernelProviderFactory + Upgrade to Juzu 0.6.0-beta11 + Upgrade to Servlet 3

--- commons-testing/src/main/java/org/exoplatform/commons/testing/mock/MockServletContext.java
+++ commons-testing/src/main/java/org/exoplatform/commons/testing/mock/MockServletContext.java
@@ -20,12 +20,21 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.EventListener;
+import java.util.Map;
 import java.util.Set;
 
+
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import javax.servlet.descriptor.JspConfigDescriptor;
 
 /**
  * @author <a href="mailto:patrice.lamarque@exoplatform.com">Patrice Lamarque</a>
@@ -34,104 +43,204 @@ import javax.servlet.ServletException;
 public class MockServletContext implements ServletContext {
 
   String servletContextName;
-  
+
   public MockServletContext(String servletContextName) {
     this.servletContextName = servletContextName;
   }
-  
-  
-  public Object getAttribute(String name) {
 
+  public Object getAttribute(String name) {
     return null;
   }
 
   public Enumeration getAttributeNames() {
-
     return null;
   }
 
   public ServletContext getContext(String uripath) {
-
     return null;
   }
 
   public String getInitParameter(String name) {
-
     return null;
   }
 
   public Enumeration getInitParameterNames() {
-
     return null;
   }
 
-  public int getMajorVersion() {
+  @Override
+  public boolean setInitParameter(String s, String s2) {
+    return false;
+  }
 
+  public int getMajorVersion() {
     return 0;
   }
 
   public String getMimeType(String file) {
-
     return null;
   }
 
   public int getMinorVersion() {
+    return 0;
+  }
 
+  @Override
+  public int getEffectiveMajorVersion() {
+    return 0;
+  }
+
+  @Override
+  public int getEffectiveMinorVersion() {
     return 0;
   }
 
   public RequestDispatcher getNamedDispatcher(String name) {
-
     return null;
   }
 
   public String getRealPath(String path) {
-
     return null;
   }
 
   public RequestDispatcher getRequestDispatcher(String path) {
-
     return null;
   }
 
   public URL getResource(String path) throws MalformedURLException {
-
     return null;
   }
 
   public InputStream getResourceAsStream(String path) {
-
     return null;
   }
 
   public Set getResourcePaths(String path) {
-
     return null;
   }
 
   public String getServerInfo() {
-
     return null;
   }
 
   public Servlet getServlet(String name) throws ServletException {
-
     return null;
   }
 
   public String getServletContextName() {
-
     return servletContextName;
   }
 
-  public Enumeration getServletNames() {
+  @Override
+  public ServletRegistration.Dynamic addServlet(String s, String s2) {
+    return null;
+  }
 
+  @Override
+  public ServletRegistration.Dynamic addServlet(String s, Servlet servlet) {
+    return null;
+  }
+
+  @Override
+  public ServletRegistration.Dynamic addServlet(String s, Class<? extends Servlet> aClass) {
+    return null;
+  }
+
+  @Override
+  public <T extends Servlet> T createServlet(Class<T> tClass) throws ServletException {
+    return null;
+  }
+
+  @Override
+  public ServletRegistration getServletRegistration(String s) {
+    return null;
+  }
+
+  @Override
+  public Map<String, ? extends ServletRegistration> getServletRegistrations() {
+    return null;
+  }
+
+  @Override
+  public FilterRegistration.Dynamic addFilter(String s, String s2) {
+    return null;
+  }
+
+  @Override
+  public FilterRegistration.Dynamic addFilter(String s, Filter filter) {
+    return null;
+  }
+
+  @Override
+  public FilterRegistration.Dynamic addFilter(String s, Class<? extends Filter> aClass) {
+    return null;
+  }
+
+  @Override
+  public <T extends Filter> T createFilter(Class<T> tClass) throws ServletException {
+    return null;
+  }
+
+  @Override
+  public FilterRegistration getFilterRegistration(String s) {
+    return null;
+  }
+
+  @Override
+  public Map<String, ? extends FilterRegistration> getFilterRegistrations() {
+    return null;
+  }
+
+  @Override
+  public SessionCookieConfig getSessionCookieConfig() {
+    return null;
+  }
+
+  @Override
+  public void setSessionTrackingModes(Set<SessionTrackingMode> sessionTrackingModes) {
+
+  }
+
+  @Override
+  public Set<SessionTrackingMode> getDefaultSessionTrackingModes() {
+    return null;
+  }
+
+  @Override
+  public Set<SessionTrackingMode> getEffectiveSessionTrackingModes() {
+    return null;
+  }
+
+  @Override
+  public void addListener(String s) {
+
+  }
+
+  @Override
+  public <T extends EventListener> void addListener(T t) {
+
+  }
+
+  @Override
+  public void addListener(Class<? extends EventListener> aClass) {
+
+  }
+
+  @Override
+  public <T extends EventListener> T createListener(Class<T> tClass) throws ServletException {
+    return null;
+  }
+
+  @Override
+  public JspConfigDescriptor getJspConfigDescriptor() {
+    return null;
+  }
+
+  public Enumeration getServletNames() {
     return null;
   }
 
   public Enumeration getServlets() {
-
     return null;
   }
 
@@ -150,9 +259,16 @@ public class MockServletContext implements ServletContext {
   public void setAttribute(String name, Object object) {
   }
 
-
   public String getContextPath() {
-    
+    return null;
+  }
+
+  @Override
+  public void declareRoles(String... strings) {
+  }
+
+  @Override
+  public ClassLoader getClassLoader() {
     return null;
   }
 
