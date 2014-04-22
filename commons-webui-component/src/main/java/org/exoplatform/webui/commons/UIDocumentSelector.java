62313b00f3aecb3c32e86375936f85f6168743e5
azayati <ayoub.ezzayati@exoplatform.com>
2014-04-22 18:51:02 +0200

    Update UIDocumentSelector.java

--- commons-webui-component/src/main/java/org/exoplatform/webui/commons/UIDocumentSelector.java
+++ commons-webui-component/src/main/java/org/exoplatform/webui/commons/UIDocumentSelector.java
@@ -163,7 +163,7 @@ public class UIDocumentSelector extends UIContainer {
   }
 
   protected String getRestContext() {
-	String requestURL = getRequestUrl();
+    String requestURL = getRequestUrl();
     String portalName = PortalContainer.getCurrentPortalContainerName();
     String restContextName = PortalContainer.getCurrentRestContextName();
     StringBuilder sb = new StringBuilder();
@@ -176,7 +176,7 @@ public class UIDocumentSelector extends UIContainer {
  
   protected String getRequestUrl() {
     WebuiRequestContext context = WebuiRequestContext.getCurrentInstance();
-	if (!(context instanceof PortalRequestContext)) {
+    if (!(context instanceof PortalRequestContext)) {
       context = (WebuiRequestContext) context.getParentAppRequestContext();
     }
     return ((PortalRequestContext) context).getRequest().getRequestURL().toString();
