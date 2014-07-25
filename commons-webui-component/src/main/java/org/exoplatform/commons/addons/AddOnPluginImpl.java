285bcc8f802ced6143d3090e8bba00bc06244700
phuong_vu <phuong.vu@exoplatform.com>
2014-07-25 18:12:47 +0700

    COMMONS-341 Support to config portlet preferences

--- commons-webui-component/src/main/java/org/exoplatform/commons/addons/AddOnPluginImpl.java
+++ commons-webui-component/src/main/java/org/exoplatform/commons/addons/AddOnPluginImpl.java
@@ -19,6 +19,7 @@
 
 package org.exoplatform.commons.addons;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -26,6 +27,8 @@ import java.util.List;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.container.xml.ValueParam;
 import org.exoplatform.portal.config.model.Application;
+import org.exoplatform.portal.config.model.TransientApplicationState;
+import org.exoplatform.portal.pom.spi.portlet.PortletBuilder;
 
 public class AddOnPluginImpl extends AddOnPlugin {
 
@@ -47,6 +50,23 @@ public class AddOnPluginImpl extends AddOnPlugin {
 
             List<Application<?>> tmp = params.<Application<?>>getObjectParamValues((Class<Application<?>>)(Class<?>)Application.class);
             if (tmp != null) {
+                for (Application<?> app : tmp) {
+                    if (app.getState() instanceof TransientApplicationState) {
+                        TransientApplicationState state = (TransientApplicationState)app.getState();
+                        if (state != null && state.getContentState() != null) {
+                            PortletBuilder builder = new PortletBuilder();
+                            String prefs = state.getContentState().toString();                            
+                            for (String pref : prefs.trim().split(";")) {
+                                String[] p = pref.split("=");
+                                if (p.length == 2) {
+                                    String[] values = p[1].split(",");
+                                    builder.add(p[0], Arrays.asList(values));
+                                }
+                            }
+                            state.setContentState(builder.build());
+                        }                        
+                    }
+                }
                 apps.addAll(tmp);
             }
         }
