b48f9e01c38730140dcf1dd8cee5e6cedbad41a5
thanhvc <thanhvc@exoplatform.com>
2015-12-15 09:25:40 +0700

    FORUM-1211 | Implements method to encode script tag in commons to resolve XSS secturity

---
+++ commons-component-common/src/main/java/org/exoplatform/commons/utils/StringCommonUtils.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2003-2015 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.commons.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : eXoPlatform
+ *          exo@exoplatform.com
+ * Dec 15, 2015  
+ */
+public class StringCommonUtils {
+
+  private static final Pattern SCRIPT_TAG_PATTERN = Pattern.compile("<(/)?[ ]*script[^>]*>", Pattern.CASE_INSENSITIVE);
+  
+  /**
+   * Encode the XSS script
+   *  
+   * @param input the given string to encode
+   * 
+   * example: "<p><Script>alert(1);</script>bbbb</p>";
+   * CommonUtils.encodeScriptMarkup(input);
+   * result = "<p>&lt;Script&gt;alert(1);&lt;&#x2f;script&gt;bbbb</p>";
+   * @return Only encode the <script> tag. 
+   */
+  public static String encodeScriptMarkup(String input) {
+    if (input != null) {
+      Matcher matcher = SCRIPT_TAG_PATTERN.matcher(input);
+      StringBuffer str = new StringBuffer(input.length());
+      while (matcher.find()) {
+        matcher.appendReplacement(str, HTMLEntityEncoder.getInstance().encodeHTMLAttribute(matcher.group()));
+      }
+      matcher.appendTail(str);
+      input = str.toString();
+    }
+    return input;
+  }
+}
---
+++ commons-component-common/src/test/java/org/exoplatform/commons/utils/StringCommonUtilsTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2003-2015 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.commons.utils;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : eXoPlatform
+ *          exo@exoplatform.com
+ * Dec 15, 2015  
+ */
+public class StringCommonUtilsTest extends TestCase {
+  
+  public void testEncodeScriptMarkup() {
+    //
+    String input = "<p><Script>alert(1);</script>bbbb</p>";
+    String done = StringCommonUtils.encodeScriptMarkup(input);
+    assertEquals("<p>&lt;Script&gt;alert(1);&lt;&#x2f;script&gt;bbbb</p>", done);
+    
+    String input1 = "<p><Script>alert(1);</SCRIPT>bbbb</p>";
+    String done1 = StringCommonUtils.encodeScriptMarkup(input1);
+    assertEquals("<p>&lt;Script&gt;alert(1);&lt;&#x2f;SCRIPT&gt;bbbb</p>", done1);
+  }
+
+}
