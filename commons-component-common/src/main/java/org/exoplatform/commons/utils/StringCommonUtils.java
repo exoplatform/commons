36dc67a92943d87343ecb0610f1a10f5b54d6c9f
thanhvc <thanhvc@exoplatform.com>
2015-12-22 15:14:45 +0700

    FORUM-1211 | [Security] XSS issue while creating topic/post

--- commons-component-common/src/main/java/org/exoplatform/commons/utils/StringCommonUtils.java
+++ commons-component-common/src/main/java/org/exoplatform/commons/utils/StringCommonUtils.java
@@ -16,6 +16,8 @@
  */
 package org.exoplatform.commons.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,7 +29,18 @@ import java.util.regex.Pattern;
  */
 public class StringCommonUtils {
 
-  private static final Pattern SCRIPT_TAG_PATTERN = Pattern.compile("(<(/|)?[ ]*(script|img|a|iframe)>|<(a|img|iframe))", Pattern.CASE_INSENSITIVE);
+  private static final Pattern SCRIPT_TAG_PATTERN = Pattern.compile("(<(/|)?[ ]*(script|img|a|iframe|object|embed)>|<(a|img|iframe|object|embed)|((on|background|src|expression).*(\"|'))|((javascript).*(;)))",
+                                                                    Pattern.CASE_INSENSITIVE);
+
+  @SuppressWarnings("serial")
+  private static List<String> MATCHED_WORDS = new ArrayList<String>() {
+    {
+      add("on");
+      add("javascript");
+      add("background");
+      add("src");
+    }
+  };
   
   /**
    * Encode the XSS script
@@ -44,11 +57,29 @@ public class StringCommonUtils {
       Matcher matcher = SCRIPT_TAG_PATTERN.matcher(input);
       StringBuffer str = new StringBuffer(input.length());
       while (matcher.find()) {
-        matcher.appendReplacement(str, HTMLEntityEncoder.getInstance().encodeHTMLAttribute(matcher.group()));
+        //removes in the case matched in the word list
+        if (matchedWord(matcher.group())) {
+          matcher.appendReplacement(str, "");
+        } else {
+          matcher.appendReplacement(str, HTMLEntityEncoder.getInstance().encodeHTMLAttribute(matcher.group()));
+        }
       }
       matcher.appendTail(str);
       input = str.toString();
     }
     return input;
+  }
+  
+  private static boolean matchedWord(String input) {
+    if (input == null || input.length() == 0) return true;
+    //
+    String lowerStr = input.toLowerCase();
+    for (String word : MATCHED_WORDS) {
+      if (lowerStr.contains(word)) {
+        return true;
+      }
+    }
+    
+    return false;
   }
 }
