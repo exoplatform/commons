1fcbb4c73da1048d37796f6a6430a2d85475def7
ayoub zayati <ayoub.ezzayati@exoplatform.com>
2016-08-16 12:44:33 +0100

    Update UserSettingServiceImpl.java
    Signed-off-by: ayoub zayati <ayoub.ezzayati@exoplatform.com>

--- commons-component-common/src/main/java/org/exoplatform/commons/notification/impl/setting/UserSettingServiceImpl.java
+++ commons-component-common/src/main/java/org/exoplatform/commons/notification/impl/setting/UserSettingServiceImpl.java
@@ -428,12 +428,21 @@ public class UserSettingServiceImpl extends AbstractService implements UserSetti
    * @throws Exception
    */
   private UserSetting fillModel(Node node) throws Exception {
-	Node parentNode = node.getParent();
-    if(!parentNode.hasProperty(EXO_LAST_MODIFIED_DATE) && parentNode.canAddMixin(EXO_MODIFY)){
-      parentNode.addMixin(EXO_MODIFY);
-      parentNode.setProperty(EXO_LAST_MODIFIED_DATE, Calendar.getInstance());
-      parentNode.save();
-	}
+    Node parentNode = node.getParent();
+    if(!parentNode.hasProperty(EXO_LAST_MODIFIED_DATE)) {
+      if(parentNode.isNodeType(EXO_MODIFY)) {
+        parentNode.setProperty(EXO_LAST_MODIFIED_DATE, Calendar.getInstance());
+        parentNode.save();
+      }
+      else if(parentNode.canAddMixin(EXO_MODIFY)) {
+        parentNode.addMixin(EXO_MODIFY);
+        parentNode.setProperty(EXO_LAST_MODIFIED_DATE, Calendar.getInstance());
+        parentNode.save();
+      }
+      else {
+        LOG.warn("Cannot add mixin to node '{}'.", parentNode.getPath());
+      }
+    }
     UserSetting model = UserSetting.getInstance();
     model.setUserId(parentNode.getName());
     model.setDailyPlugins(getValues(node, EXO_DAILY));
@@ -446,7 +455,11 @@ public class UserSettingServiceImpl extends AbstractService implements UserSetti
       model.setChannelPlugins(channel.getId(), getValues(node, getChannelProperty(channel.getId())));
     }
     //
-    model.setLastUpdateTime(parentNode.getProperty(EXO_LAST_MODIFIED_DATE).getDate());
+    if(parentNode.hasProperty(EXO_LAST_MODIFIED_DATE) ){
+      model.setLastUpdateTime(parentNode.getProperty(EXO_LAST_MODIFIED_DATE).getDate());
+    } else {
+      model.setLastUpdateTime(Calendar.getInstance());
+    }
     //
     if (node.hasProperty(EXO_IS_ENABLED)) {
       model.setEnabled(Boolean.valueOf(node.getProperty(EXO_IS_ENABLED).getString()));
@@ -481,10 +494,19 @@ public class UserSettingServiceImpl extends AbstractService implements UserSetti
         NodeIterator iter = getDefaultDailyIterator(sProvider, offset, limit);
         while (iter.hasNext()) {
           Node node = iter.nextNode();
-          if(!node.hasProperty(EXO_LAST_MODIFIED_DATE) && node.canAddMixin(EXO_MODIFY)){
-            node.addMixin(EXO_MODIFY);
-            node.setProperty(EXO_LAST_MODIFIED_DATE, Calendar.getInstance());
-            node.save();
+          if(!node.hasProperty(EXO_LAST_MODIFIED_DATE)) {
+            if(node.isNodeType(EXO_MODIFY)) {
+              node.setProperty(EXO_LAST_MODIFIED_DATE, Calendar.getInstance());
+              node.save();
+            }
+            else if(node.canAddMixin(EXO_MODIFY)) {
+              node.addMixin(EXO_MODIFY);
+              node.setProperty(EXO_LAST_MODIFIED_DATE, Calendar.getInstance());
+              node.save();
+            }
+            else {
+              LOG.warn("Cannot add mixin to node '{}'.", node.getPath());
+            }
           }
           users.add(UserSetting.getInstance()
                     .setUserId(node.getName())
