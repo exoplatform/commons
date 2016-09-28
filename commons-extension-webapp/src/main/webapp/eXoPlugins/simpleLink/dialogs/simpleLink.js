7ab9d154f28b11d7b274662c5e389932a506d227
phuong_vu <phuong.vu@exoplatform.com>
2016-09-28 12:17:02 +0700

    [SOC-5266]: [URL_TLBR_05]: The user can select a text, and by a right click in the mouse, he can specify it to a link by choosing 'Link'. the popup of a link will display to add the URL chosen.

--- commons-extension-webapp/src/main/webapp/eXoPlugins/simpleLink/dialogs/simpleLink.js
+++ commons-extension-webapp/src/main/webapp/eXoPlugins/simpleLink/dialogs/simpleLink.js
@@ -12,25 +12,57 @@ CKEDITOR.dialog.add( 'simpleLinkDialog', function( editor ) {
                         type: 'textarea',
                         id: 'text',
                         label: 'Text',
-                        validate: CKEDITOR.dialog.validate.notEmpty( "Text field cannot be empty." )
+                        validate: CKEDITOR.dialog.validate.notEmpty( "Text field cannot be empty." ),
+                        setup: function(element) {
+                            this.setValue(element.getText());
+                        },
+                        commit: function(element) {
+                            element.setText(this.getValue());
+                        }
                     },
                     {
                         type: 'text',
                         id: 'link',
                         label: 'Link',
-                        validate: CKEDITOR.dialog.validate.notEmpty( "Link field cannot be empty." )
+                        validate: CKEDITOR.dialog.validate.notEmpty( "Link field cannot be empty." ),
+                        setup: function(element) {
+                            this.setValue(element.getAttribute("href") );
+                        },
+                        commit: function(element) {
+                            element.setAttribute("href", this.getValue());
+                        }
                     }
                 ]
             }
         ],
+        
+        onShow: function() {
+            var selection = editor.getSelection();
+            var element = selection.getStartElement();
+
+            if ( element )
+                element = element.getAscendant( 'a', true );
+
+            if ( !element || element.getName() != 'a' ) {
+                element = editor.document.createElement( 'a' );
+                element.setText(selection.getSelectedText());
+                this.insertMode = true;
+            }
+            else
+                this.insertMode = false;
+
+            this.element = element;
+//            if (!this.insertMode )
+                this.setupContent( this.element );
+        },
+        
         onOk: function() {
             var dialog = this;
+            var a = this.element;
+            this.commitContent(a);
 
-            var simpleLink = editor.document.createElement( 'a' );
-            simpleLink.setAttribute( 'href', dialog.getValueOf( 'tab', 'link' ) );
-            simpleLink.setText( dialog.getValueOf( 'tab', 'text' ) );
-
-            editor.insertElement(simpleLink);
+            if ( this.insertMode )
+                editor.insertElement(a);
         }
     };
 });
--- commons-extension-webapp/src/main/webapp/eXoPlugins/simpleLink/plugin.js
+++ commons-extension-webapp/src/main/webapp/eXoPlugins/simpleLink/plugin.js
@@ -7,6 +7,22 @@ CKEDITOR.plugins.add( 'simpleLink',
             label: 'Link',
             command: 'simpleLink',
         });
+        
+        if ( editor.contextMenu ) {
+            editor.addMenuGroup( 'linkGroup' );
+            editor.addMenuItem( 'linkItem', {
+                label: 'Link',
+                icon: this.path + 'icons/simpleLink.png',
+                command: 'simpleLink',
+                group: 'linkGroup'
+            });
+
+            editor.contextMenu.addListener( function( element ) {
+                if ( element) {
+                    return { linkItem: CKEDITOR.TRISTATE_OFF };
+                }
+            });
+        };
 
         CKEDITOR.dialog.add( 'simpleLinkDialog', this.path + 'dialogs/simpleLink.js' );
     }
