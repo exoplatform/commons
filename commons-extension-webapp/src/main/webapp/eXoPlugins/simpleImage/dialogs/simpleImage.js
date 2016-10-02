37bce13a7183ef11038721a1ee84229e24d8895e
phuong_vu <phuong.vu@exoplatform.com>
2016-10-02 10:30:51 +0700

    [SOC-5266]: [IMAGE_03]: Construct image popup

--- commons-extension-webapp/src/main/webapp/eXoPlugins/simpleImage/dialogs/simpleImage.js
+++ commons-extension-webapp/src/main/webapp/eXoPlugins/simpleImage/dialogs/simpleImage.js
@@ -1,6 +1,6 @@
 CKEDITOR.dialog.add( 'simpleImageDialog', function( editor ) {
     return {
-        title: 'Link',
+        title: 'Select Picture',
         minWidth: 400,
         minHeight: 200,
         contents: [
@@ -9,10 +9,10 @@ CKEDITOR.dialog.add( 'simpleImageDialog', function( editor ) {
                 label: 'Link',
                 elements: [
                     {
-                        type: 'textarea',
-                        id: 'text',
-                        label: 'Text',
-                        validate: CKEDITOR.dialog.validate.notEmpty( "Text field cannot be empty." ),
+                        type: 'text',
+                        id: 'imgUrl',
+                        label: 'Image URL',
+                        validate: CKEDITOR.dialog.validate.notEmpty( "Image URL cannot be empty." ),
                         setup: function(element) {
                             this.setValue(element.getText());
                         },
@@ -22,15 +22,52 @@ CKEDITOR.dialog.add( 'simpleImageDialog', function( editor ) {
                     },
                     {
                         type: 'text',
-                        id: 'link',
-                        label: 'Link',
-                        validate: CKEDITOR.dialog.validate.notEmpty( "Link field cannot be empty." ),
+                        id: 'description',
+                        label: 'Description',
+                        validate: CKEDITOR.dialog.validate.notEmpty( "Description cannot be empty." ),
                         setup: function(element) {
                             this.setValue(element.getAttribute("href") );
                         },
                         commit: function(element) {
                             element.setAttribute("href", this.getValue());
                         }
+                    },
+                    {
+                        type: 'select',
+                        id: 'aligment',
+                        label: 'Aligment',
+                        widths: [ '35%', '65%' ],
+                        style: 'width:90px',
+                        'default': 'left',
+                        items: [
+                            [ editor.lang.common.alignLeft, 'left' ],
+                            [ editor.lang.common.alignRight, 'right' ]
+                        ],
+                        validate: CKEDITOR.dialog.validate.notEmpty( "Aligment cannot be empty." ),
+                        setup: function(element) {
+                            this.setValue(element.getAttribute("href") );
+                        },
+                        commit: function(element) {
+                            element.setAttribute("href", this.getValue());
+                        }
+                    },
+                    {
+                        type: 'vbox',
+                        height: '250px',
+                        children: [ {
+                            type: 'html',
+                            id: 'htmlPreview',
+                            style: 'width:95%;',
+                            html: '<div>' + CKEDITOR.tools.htmlEncode( editor.lang.common.preview ) + '<br>' +
+                                '<div id="' + 'imagePreviewLoaderId' + '" class="ImagePreviewLoader" style="display:none"><div class="loading">&nbsp;</div></div>' +
+                                '<div><table><tr><td>' +
+                                    '<a href="javascript:void(0)" target="_blank" onclick="return false;" id="' + 'previewLinkId' + '">' +
+                                    '<img id="' + 'previewImageId' + '" alt="" /></a>' +
+                                // jscs:disable maximumLineLength
+                                    ("") +
+                                // jscs:enable maximumLineLength
+                                '</td></tr></table></div></div>'
+                        } ]
                     }
                 ]
             }
