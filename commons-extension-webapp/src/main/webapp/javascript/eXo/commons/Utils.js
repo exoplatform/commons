50961b480d1a8f97b5672d0a1fb4fcdb54538f67
Khemais <kmenzli@exoplatform.com>
2016-10-20 09:50:09 +0100

    CAL-1255: The URL in event description displayed in an Event activity is not clickable

--- commons-extension-webapp/src/main/webapp/javascript/eXo/commons/Utils.js
+++ commons-extension-webapp/src/main/webapp/javascript/eXo/commons/Utils.js
@@ -19,7 +19,7 @@
     if (isMobile) {
       body.addClass('phoneDisplay');
     }
-	if (isSmallMobile) {
+    if (isSmallMobile) {
       body.addClass('phoneSmallDisplay');
     }
     if (isTablet) {
@@ -29,8 +29,30 @@
       body.addClass('tabletLDisplay');
     }
     return {'isMobile' : isMobile,'isSmallMobile' : isSmallMobile, 'isTablet' : isTablet, 'isTabletL' : isTabletL};
-  }
-
+  };
+  Utils.prototype.urlify = function(text){
+    return text.replace(/((((https?|ftp|file):\/\/)|www\.)[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])/ig, function(url){
+      var value = url;
+      if(url.indexOf('www.') == 0) {
+        url = 'http://' + url;
+      }
+      return '<a href="' + url + '" target="_blank">' + value + '</a>';
+    })
+  };
+  Utils.prototype.fetchElementByConfig = function(params){
+    Utils.activityId = params.activityId || null;
+    Utils.targetDiv = params.targetDiv || "";
+    Utils.targetElement = params.targetElement || "";
+    if (Utils.activityId == null) {
+      return;
+    }
+    Utils.wrapDiv = Utils.targetDiv + Utils.activityId;
+    var descriptionValue = gj('div#'+Utils.wrapDiv).find('.'+Utils.targetElement).first('p').text();
+    //--- Urlify description
+    descriptionValue = this.urlify(descriptionValue);
+    //--- Update description output
+    gj('div#'+Utils.wrapDiv).find('.'+Utils.targetElement).first('p').html( descriptionValue );
+  };
   if(!eXo.commons) eXo.commons={};
   eXo.commons.Utils = new Utils();
   return {
