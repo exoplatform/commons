df04dc5cc27e6eeb8db96178d2783d74724196bf
Aymen Boughzela <aboughzela@exoplatform.com>
2016-12-14 12:41:38 +0100

    COMMONS-509  : Cluster Aware API

---
+++ commons-component-common/src/main/java/org/exoplatform/commons/cluster/StartableClusterAware.java
@@ -0,0 +1,20 @@
+package org.exoplatform.commons.cluster;
+
+/**
+ * A class implementing this interface is executed will be started (execution of the method start())
+ * during the startup of the platform but on one node only if running in cluster.
+ * If the node running the cluster aware service is stopped during the execution of the service,
+ * it will be started again on another cluster node, unless the isDone() method returns true.
+ */
+public interface StartableClusterAware {
+    /**
+     * Start service by the current node
+     */
+    void start();
+
+    /**
+     * Check if service is done
+     * @return
+     */
+    boolean isDone();
+}
