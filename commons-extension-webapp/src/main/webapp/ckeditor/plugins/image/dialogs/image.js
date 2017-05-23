7a4bd4a31765433137a3283ba7c61e51a83dbd5a
AtiaZ <AtiaZ@users.noreply.github.com>
2017-05-23 16:23:30 +0100

    [ECMS-7625] Upload an image through CKeditor in the media folder of a… (#123)

--- commons-extension-webapp/src/main/webapp/ckeditor/plugins/image/dialogs/image.js
+++ commons-extension-webapp/src/main/webapp/ckeditor/plugins/image/dialogs/image.js
@@ -565,8 +565,23 @@
 								align: 'center',
 								label: editor.lang.common.browseServer,
 								onClick: function(){
-									window.open('/eXoWCMResources/eXoPlugins/content/content.html?insertContentType=Image&viewType=thumbnail&components='+previewImageId+'&currentInstance='+editor.name,'WCMGadgetSelector','width=1024,height=600');
-									window.popup_opened = true;
+                                                                       var nodePath = null;
+                                                                       var addressBarContent = document.getElementById('AddressBarControl').children[1].defaultValue;
+                                                                       var subPath = decodeURIComponent(addressBarContent);
+                                                                       //add this condition to make the difference between the path of a WebContent which already exists and a new one
+                                                                       if ( document.getElementsByClassName('addressItem')  && document.getElementsByClassName('addressItem').length > 0) {
+                                                                           var addressItem = document.getElementsByClassName('addressItem')["0"].childNodes["0"].data;
+                                                                           //to avoid the duplication of addressItem in case when trying to extract the nodePath of some drives (like Managed Sites) when access to them from the collaboration drive
+                                                                           if ( ! ( addressBarContent.startsWith(addressItem))) {
+                                                                                   nodePath = decodeURIComponent(addressItem + addressBarContent);
+                                                                            } else {
+                                                                                   nodePath = decodeURIComponent(addressBarContent).replace(decodeURIComponent(addressItem), "");
+                                                                            }
+                                                                       } else {
+                                                                            nodePath = decodeURIComponent(addressBarContent);
+                                                                       }
+                                                                       window.open('/eXoWCMResources/eXoPlugins/content/content.html?insertContentType=Image&subPath='+subPath+'&nodePath=' + nodePath + '&viewType=thumbnail&components=' + previewImageId + '&currentInstance=' + editor.name, 'WCMGadgetSelector', 'width=1024,height=600');
+                                                                       window.popup_opened = true;
 								},
 								hidden: false,
 								filebrowser: 'info:txtUrl'
