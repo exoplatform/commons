5c2250cf3ba65a91bff701f73d20e6bb36ee9d94
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2018-12-18 14:13:31 +0100

    PLF-8237 : add method in XSSUtils to escape HTML entities (#245)
    Add a method escapeHtml in XSSUtils to escape HTML entities.

--- commons-extension-webapp/src/main/webapp/javascript/eXo/commons/XSSUtils.js
+++ commons-extension-webapp/src/main/webapp/javascript/eXo/commons/XSSUtils.js
@@ -1,7 +1,18 @@
 (function () {
   var XSSUtils = {
-    sanitizeString : function (keyword) {
+    sanitizeString: function (keyword) {
       return keyword.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;").replace(/\//g, "&#x2F;");
+    },
+    escapeHtml: function(html) {
+      var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+      };
+
+      return html.replace(/[&<>"']/g, function(m) { return map[m]; });
     }
   };
     
