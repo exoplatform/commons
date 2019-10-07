3df19e3926f6621f4d5e89c5323b5f2554bfc6c1
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2019-10-07 18:09:47 +0100

    SOC-6263 : Fix build failure on Commons project (#290)

--- commons-component-common/src/test/java/org/exoplatform/commons/utils/DateUtilsTest.java
+++ commons-component-common/src/test/java/org/exoplatform/commons/utils/DateUtilsTest.java
@@ -18,10 +18,11 @@
  */
 package org.exoplatform.commons.utils;
 
-import junit.framework.TestCase;
+import org.exoplatform.commons.testing.BaseCommonsTestCase;
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
 import org.junit.Test;
 
-import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -30,7 +31,9 @@ import java.util.TimeZone;
  * Author : Aymen Boughzela
  *          aboughzela@exoplatform.com
  */
-public class DateUtilsTest extends TestCase {
+public class DateUtilsTest extends BaseCommonsTestCase {
+    ExoContainer container = new ExoContainer();
+    
     public void testGetTimeZone() {
         assertEquals(TimeZone.getTimeZone("GMT"), DateUtils.getTimeZone("GMT"));
         assertEquals(TimeZone.getTimeZone("Africa/Tunis"), DateUtils.getTimeZone("Africa/Tunis"));
@@ -45,10 +48,14 @@ public class DateUtilsTest extends TestCase {
 
     @Test
     public void testGetRelativeTimeLabel() {
-        assertEquals("less than a minute ago", DateUtils.getRelativeTimeLabel(Locale.ENGLISH, System.currentTimeMillis() - 30L));
-        assertEquals("about a month ago", DateUtils.getRelativeTimeLabel(Locale.ENGLISH, System.currentTimeMillis() - 3000000000L));
-        assertEquals("about 2 months ago", DateUtils.getRelativeTimeLabel(Locale.ENGLISH, System.currentTimeMillis() - 7000000000L));
-        assertEquals("about 3 months ago", DateUtils.getRelativeTimeLabel(Locale.ENGLISH, System.currentTimeMillis() - 10000000000L));
-
+        ExoContainerContext.setCurrentContainer(container);
+        try {
+            assertEquals("less than a minute ago", DateUtils.getRelativeTimeLabel(Locale.ENGLISH, System.currentTimeMillis() - 30L));
+            assertEquals("about a month ago", DateUtils.getRelativeTimeLabel(Locale.ENGLISH, System.currentTimeMillis() - 3000000000L));
+            assertEquals("about 2 months ago", DateUtils.getRelativeTimeLabel(Locale.ENGLISH, System.currentTimeMillis() - 7000000000L));
+            assertEquals("about 3 months ago", DateUtils.getRelativeTimeLabel(Locale.ENGLISH, System.currentTimeMillis() - 10000000000L));
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 }
