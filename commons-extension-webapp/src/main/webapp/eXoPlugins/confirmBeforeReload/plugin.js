f902d0c4ac0b5ecee5b33cb055a4a1949c331d0b
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2019-11-18 09:16:22 +0100

    PLF-8604 Fix JS error on comment editing (#303)

--- commons-extension-webapp/src/main/webapp/eXoPlugins/confirmBeforeReload/plugin.js
+++ commons-extension-webapp/src/main/webapp/eXoPlugins/confirmBeforeReload/plugin.js
@@ -1,17 +1,24 @@
 require(['SHARED/jquery'],function($) {
   function handleBeforeUnload(editor) {
-    $(window).on( 'beforeunload', evt => {
-      if ( $(`.${editor.id}`).length && $(`.${editor.id}`).is(":visible") && editor.getData().trim() && editor.checkDirty() ) {
-        return false;
-      }
-    });
+    if (editor && !editor.beforeReloadHandled) {
+      editor.beforeReloadHandled = true;
+      $(window).on( 'beforeunload', evt => {
+        if (editor) {
+          if ( $(`.${editor.id}`).length && $(`.${editor.id}`).is(":visible") && editor.getData().trim() && editor.checkDirty() ) {
+            return false;
+          }
+        }
+      });
+    }
   }
 
   CKEDITOR.plugins.add( 'confirmBeforeReload', {
     init : function(editor) {
-      editor.on('instanceReady', function(evt) {
-        handleBeforeUnload(editor);
-      });
+      if (editor) {
+        editor.on('instanceReady', function(evt) {
+          handleBeforeUnload(editor);
+        });
+      }
     }
   });
 });
