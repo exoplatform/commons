890c1c5103fea89de832ac3cffa3fc1ff9a8b60f
Ali HAMDI <ahamdi@exoplatform.com>
2019-11-21 22:47:25 +0100

    PLF-8447 : Add default to method signature in interface MailNotificationStorage (#306)
    * Add default to method signature in interface MailNotificationStorage
    
    * throw UnsupportedOperationException in case of using the default implementation

--- commons-api/src/main/java/org/exoplatform/commons/api/notification/service/storage/MailNotificationStorage.java
+++ commons-api/src/main/java/org/exoplatform/commons/api/notification/service/storage/MailNotificationStorage.java
@@ -56,6 +56,8 @@ public interface MailNotificationStorage {
    *
    * @throws Exception
    */
-  void deleteAllDigests() throws Exception;
-  
+  default void deleteAllDigests() throws Exception {
+    throw new UnsupportedOperationException();
+  }
+
 }
