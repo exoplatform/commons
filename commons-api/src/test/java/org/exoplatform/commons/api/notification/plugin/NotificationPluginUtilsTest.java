a0d254b0658118e12720fea1341fe54d03e692eb
Moez Diha <38471028+mdiha@users.noreply.github.com>
2019-12-05 09:52:12 +0100

    PLF-8566: Enable the change of title and subject for Daily and weekly digest email (#309)
    Issue: Title and subject of Daily and weekly digest email is not coherent with company name,
    after digging in this issue i found out that the method getBrandingPortalName always returns the default company name which is "eXo".
    To fix this issue i made sure to retrieve company name from the company branding storage.

--- commons-api/src/test/java/org/exoplatform/commons/api/notification/plugin/NotificationPluginUtilsTest.java
+++ commons-api/src/test/java/org/exoplatform/commons/api/notification/plugin/NotificationPluginUtilsTest.java
@@ -2,12 +2,19 @@ package org.exoplatform.commons.api.notification.plugin;
 
 import junit.framework.TestCase;
 
+import org.exoplatform.commons.api.settings.SettingService;
+import org.exoplatform.commons.api.settings.SettingValue;
+import org.exoplatform.commons.api.settings.data.Context;
+import org.exoplatform.commons.api.settings.data.Scope;
+import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.services.organization.OrganizationService;
 import org.exoplatform.services.organization.UserProfile;
 import org.exoplatform.services.organization.UserProfileHandler;
 import org.exoplatform.services.organization.impl.UserProfileImpl;
-import org.exoplatform.services.resources.LocalePolicy;
+import org.mockito.Mockito;
+
+import static org.exoplatform.commons.api.notification.plugin.NotificationPluginUtils.BRANDING_COMPANY_NAME_SETTING_KEY;
 
 
 /**
@@ -37,5 +44,19 @@ public class NotificationPluginUtilsTest extends TestCase {
 
     language = NotificationPluginUtils.getLanguage(userName);
     assertEquals(langFR_FR, language);
+  }
+
+  public void testGetBrandingPortalName() throws Exception {
+    SettingService settingService = Mockito.mock(SettingService.class);
+    PortalContainer container = PortalContainer.getInstance();
+    container.unregisterComponent(SettingService.class);
+    container.registerComponentInstance(SettingService.class, settingService);
+
+    String companyName = "ACME";
+    SettingValue value = new SettingValue(companyName);
+    Mockito.when(settingService.get(Context.GLOBAL, Scope.GLOBAL, BRANDING_COMPANY_NAME_SETTING_KEY)).thenReturn(value);
+    
+    // Make sure that method getBrandingPortalName returns the changed company name.
+    assertEquals(companyName, NotificationPluginUtils.getBrandingPortalName());
   }
 }
